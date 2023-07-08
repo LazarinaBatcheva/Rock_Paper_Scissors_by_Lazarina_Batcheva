938e187538e2b3f0d7f71b47fff72461cdf6a769
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2023-07-08 22:08:11 +0200

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,34 +1,35 @@
 import random
+from colorama import Fore
 
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
 player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
-if player_move == "r":
+if player_move.lawer() == "r":
     player_move = rock
-elif player_move == "p":
+elif player_move.lawer() == "p":
     player_move = paper
-elif player_move == "s":
+elif player_move.lawer() == "s":
     player_move = scissors
 else:
     raise SystemExit("Invalid input. Try again...")
 
 computer_random_number = random.randint(1, 3)
 computer_move = ""
-if computer_move == 1:
+if computer_random_number == 1:
     computer_move = rock
-elif computer_move == 2:
+elif computer_random_number == 2:
     computer_move = paper
 else:
     computer_move = scissors
-print(f"The computer chose {computer_move}.")
+print(Fore.BLUE + f"\033[3mThe computer chose {computer_move}.\033[0m")
 
 if (player_move == rock and computer_move == scissors) or \
     (player_move == paper and computer_move == rock) or \
     (player_move == scissors and computer_move == paper):
-    print("You win!")
+    print(Fore.GREEN + "\033[1m\033[3mYou win!\033[0m")
 elif player_move == computer_move:
-    print("Draw!")
+    print(Fore.YELLOW + "\033[1m\033[3mDraw!\033[0m")
 else:
-    print("You lose!")
+    print(Fore.MAGENTA + "\033[1m\033[3mYou lose!\033[0m")
